603abd4c52170a9d8b5a0d6fd2ec7371c05fe36e
nnnonly <nnnonly1407@gmail.com>
2025-04-24 17:36:05 +0200

    initial commit

---
+++ run.py
@@ -0,0 +1,18 @@
+from src.Simulator import Simulator
+
+if __name__ == '__main__':
+    sim_config_file = "xml/nfs.xml"
+    trace = True
+    verbose = True
+
+    forced_load = 200
+    num_simulations = 1
+
+    Simulator(sim_config_file, trace, verbose, forced_load, num_simulations)
+    # min_load = 75
+    # max_load = 400
+    # step = 25
+    #
+    # for forced_load in range(min_load, max_load + 1, step):
+    #     Simulator(sim_config_file, trace, verbose, forced_load, num_simulations)
+
---
+++ src/ProtectingLightPath.py
@@ -0,0 +1,39 @@
+from typing import List, Tuple, Set
+from src.Slot import Slot
+
+
+class ProtectingLightPath:
+    def __init__(self, id: float, src: int, dst: int, links_id: List[int], fss: int):
+        if id < 0 or src < 0 or dst < 0 or len(links_id) < 1 or fss < 0:
+            raise ValueError("IllegalArgumentException")
+        else:
+            self.id = id
+            self.src = src
+            self.dst = dst
+            self.links_id = links_id
+            self.fss = fss
+
+    def get_id(self) -> float:
+        return self.id
+
+    def get_source(self) -> int:
+        return self.src
+
+    def get_destination(self) -> int:
+        return self.dst
+
+    def get_links(self) -> List[int]:
+        return self.links_id
+
+    def get_fss(self) -> int:
+        return self.fss
+
+    def __str__(self) -> str:
+        protect_light_path = f"id:{self.id}, src: {self.src}, dst:{self.dst}, links: "
+        return protect_light_path
+    #
+    # def to_trace(self) -> str:
+    #     light_path = f"{self.id} {self.src} {self.dst} "
+    #     for i in range(0, len(self.links), 1):
+    #         light_path += f"{self.links[i]}-"
+    #     return light_path
